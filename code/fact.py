8e7ea581e4d687c58c5a323089262401017a16b2
Melanie Sedda <melaniesedda@gmail.com>
2015-11-20 00:49:20 +0100

    gauss jordan bitwise

--- code/fact.py
+++ code/fact.py
@@ -3,11 +3,13 @@ from datetime import datetime
 import random
 import math
 import numpy as np
-from sets import Set
+import copy
 
 print_debug = True
-print_time = False
+print_time = True
 first_factorisations_mode = True
+max_prime = 4000
+first_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987, 1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111, 2113, 2129, 2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239, 2243, 2251, 2267, 2269, 2273, 2281, 2287, 2293, 2297, 2309, 2311, 2333, 2339, 2341, 2347, 2351, 2357, 2371, 2377, 2381, 2383, 2389, 2393, 2399, 2411, 2417, 2423, 2437, 2441, 2447, 2459, 2467, 2473, 2477, 2503, 2521, 2531, 2539, 2543, 2549, 2551, 2557, 2579, 2591, 2593, 2609, 2617, 2621, 2633, 2647, 2657, 2659, 2663, 2671, 2677, 2683, 2687, 2689, 2693, 2699, 2707, 2711, 2713, 2719, 2729, 2731, 2741, 2749, 2753, 2767, 2777, 2789, 2791, 2797, 2801, 2803, 2819, 2833, 2837, 2843, 2851, 2857, 2861, 2879, 2887, 2897, 2903, 2909, 2917, 2927, 2939, 2953, 2957, 2963, 2969, 2971, 2999, 3001, 3011, 3019, 3023, 3037, 3041, 3049, 3061, 3067, 3079, 3083, 3089, 3109, 3119, 3121, 3137, 3163, 3167, 3169, 3181, 3187, 3191, 3203, 3209, 3217, 3221, 3229, 3251, 3253, 3257, 3259, 3271, 3299, 3301, 3307, 3313, 3319, 3323, 3329, 3331, 3343, 3347, 3359, 3361, 3371, 3373, 3389, 3391, 3407, 3413, 3433, 3449, 3457, 3461, 3463, 3467, 3469, 3491, 3499, 3511, 3517, 3527, 3529, 3533, 3539, 3541, 3547, 3557, 3559, 3571, 3581, 3583, 3593, 3607, 3613, 3617, 3623, 3631, 3637, 3643, 3659, 3671, 3673, 3677, 3691, 3697, 3701, 3709, 3719, 3727, 3733, 3739, 3761, 3767, 3769, 3779, 3793, 3797, 3803, 3821, 3823, 3833, 3847, 3851, 3853, 3863, 3877, 3881, 3889, 3907, 3911, 3917, 3919, 3923, 3929, 3931, 3943, 3947, 3967, 3989]
 max_dec = 1
 first_fact = ()
 
@@ -38,6 +40,18 @@ def powermod(x, n, m):
         return (y*y)%m
     else:
         return (((y*y)%m)*x)%m
+
+def power(x, n):
+    n = int(n)
+    if n == 0:
+        return 1
+    if n == 1 or x == 1:
+        return x
+    y = power(x, n/2)
+    if n & 1 == 0:
+        return y*y
+    else:
+        return y*y*x
 
 
 def miller_rabin(N):
@@ -204,25 +218,132 @@ def fermat3(N):
     return a-int(sqrtb), a+int(sqrtb)
 
 
+def find_first_primes():
+    is_prime = np.ones(max_prime, dtype=bool)
+    for i in range(2, max_prime):
+        if is_prime[i]:
+            first_primes.append(i)
+            j = i*i
+            while j < max_prime:
+                is_prime[j] = False
+                j += i
+
+
+# Compute (n/p)
+def legendre(n, p):
+    n = n % p
+    if n == 0:
+        return 0
+    ans = 1
+    # n =  2^s * m
+    s = 0
+    m = n
+    while m & 1 == 0:
+        m /= 2
+        s += 1
+    if s & 1 == 1 and (p % 8 == 3 or p % 8 == 5):
+        ans = -1
+    if m == 1:
+        return ans
+    if m % 4 == 3 and p % 4 == 3:
+        ans *= (-1)
+    return ans*legendre(p, m)
+
+
+#For p prime and n with (n/p) = 1, returns r such that r^2 = n mod p
+def tonelli_shanks(n, p):
+    if p == 2:
+        return [1]
+    n = n % p
+    if p % 4 == 3:
+        R = powermod(n, (p+1)/4, p)
+        return [R, p-R]
+    # p-1 = Q*2^S
+    S = 0
+    Q = p-1
+    while Q & 1 == 0:
+        Q /= 2
+        S += 1
+
+    z = 2
+    while legendre(z, p) != -1:
+        z += 1
+    c = powermod(z, Q, p)
+    R = powermod(n, (Q+1)/2, p)
+    t = powermod(n, Q, p)
+    M = S
+    while True:
+        if t == 1:
+            return [R, p-R]
+        i = 1
+        t2i = t*t % p
+        while t2i != 1:
+            t2i = t2i*t2i % p
+            i += 1
+        b = powermod(c, int(power(2, M-i-1)), p)
+        R = R*b % p
+        t = t*b*b % p
+        c = b*b % p
+        M = i
+
+
 def decompose(b, baseSize, base):
-    dec = np.zeros(baseSize, dtype=np.int)
-    dec_parity = np.zeros(baseSize, dtype=np.int)
+    dec = np.zeros(baseSize, dtype=np.long)
+    dec_parity = 0
     i = 0
     for f in base:
-        while b % f == 0:
-            dec[i] += 1
-            b /= f
-        dec_parity[i] = dec[i] & 1
+        if f == -1:
+            if b < 0:
+                dec[i] = 1
+                dec_parity |= 1 << (baseSize-1-i)
+        else:
+            while b % f == 0:
+                dec[i] += 1
+                b /= f
+            if dec[i] & 1 == 1:
+                dec_parity |= 1 << (baseSize-1-i)
         i += 1
     if b > 1:
         return None, None
     else:
         return dec, dec_parity
 
+def gauss_jordan_bitwise(L, nSmooth, baseSize):
+    if print_debug:
+        print("nSmooth = %d, baseSize = %d" % (nSmooth, baseSize))
+        for i in range(nSmooth):
+            if print_debug:
+                print "{0:b}".format(L[i])
+    C = [1 << nSmooth-1-i for i in range(nSmooth)]
+    if print_debug:
+        for i in range(nSmooth):
+            if print_debug:
+                print "{0:b}".format(C[i])
+    indice = [i for i in range(nSmooth)]
+    for c in range(baseSize):
+        if print_debug:
+            print("c = %d" % c)
+        # Find pivot
+        for i in range(c, nSmooth):
+            if L[indice[i]] & (1 << (baseSize-c-1)) != 0:
+                break
+        temp = indice[i]
+        indice[i] = indice[c]
+        indice[c] = temp
+        if print_debug:
+            print("The pivot is in position (%d, %d)" % (i, c))
+        # Subtract
+        for j in range(c+1, nSmooth):
+            if L[indice[j]] & (1 << (baseSize-c-1)) != 0:
+                L[indice[j]] ^= L[indice[c]]
+                C[indice[j]] ^= C[indice[c]]
+    return [C[indice[i]] for i in range(nSmooth) if L[indice[i]] == 0]
 
-def gauss_jordan(A, baseSize):
-    comb = [[i] for i in range(baseSize+1)]
-    index = [i for i in range(baseSize+1)]
+
+
+def gauss_jordan(A, nSmooth, baseSize):
+    comb = [[i] for i in range(nSmooth)]
+    index = [i for i in range(nSmooth)]
     r = -1
     for j in range(baseSize):
         if print_debug:
@@ -231,7 +352,7 @@ def gauss_jordan(A, baseSize):
             print("comb =")
             print comb
         # Find pivot
-        for k in range(r+1, baseSize+1):
+        for k in range(r+1, nSmooth):
             if A[k, j] == 1:
                 break
         if print_debug:
@@ -242,22 +363,22 @@ def gauss_jordan(A, baseSize):
              if k != r:
                  if print_debug:
                      print("k = %d, r = %d" % (k, r))
-                 temp = A[r, :]
+                 temp = copy.deepcopy(A[r, :])
                  if print_debug:
-                     print A[r, :]
-                 A[r, :] = A[k, :]
+                     print temp, A[r, :], A[k, :]
+                 A[r, :] = copy.deepcopy(A[k, :])
                  if print_debug:
-                     print A[r, :]
-                 A[k, :] = temp
+                     print temp, A[r, :], A[k, :]
+                 A[k, :] = copy.deepcopy(temp)
                  if print_debug:
-                     print A[k, :]
+                     print temp, A[r, :], A[k, :]
                  temp = index[k]
                  index[k] = index[r]
                  index[r] = temp
                  if print_debug:
                     print("A =")
                     print A
-             for i in range(baseSize+1):
+             for i in range(nSmooth):
                  if i != r:
                      if A[i, j] != 0:
                          for c in comb[index[r]]:
@@ -265,7 +386,7 @@ def gauss_jordan(A, baseSize):
                          for l in range(baseSize):
                              A[i, l] = (A[i, l] - A[r, l]) & 1
                              if print_debug:
-                                 print("entry (%d, %d) becomes %d" %(i, l, A[i, l] - A[r, j]))
+                                 print("entry (%d, %d) becomes %d" % (i, l, (A[i, l] - A[r, j]) & 1))
                          if not np.any(A[i, :] % 2):
                              if print_debug:
                                  print "zero line found"
@@ -284,18 +405,105 @@ def gauss_jordan(A, baseSize):
         print comb[baseSize]
     return comb[baseSize]
 
+
 def quadratic_sieve(N):
-     base = [2, 7, 13]
-     baseSize = 3
-     dec = np.zeros((baseSize+1, baseSize), dtype=np.int)
-     dec_parity = np.zeros((baseSize+1, baseSize), dtype=np.int)
-     a = int(math.ceil(math.sqrt(N)))
+     # Find base
+     B = math.ceil(math.pow(math.exp(math.sqrt(math.log(N)*math.log(math.log(N)))), math.sqrt(2)/4))
+     if print_debug:
+        print("B = %d" % B)
+     base = [-1, 2]
+     baseSize = 2
+     i = 1
+     while baseSize < B:
+         if print_debug:
+            print first_primes[i]
+         l = legendre(N, first_primes[i])
+         if l == 0:
+             return i, N/i
+         if l == 1:
+             if print_debug:
+                print "added"
+             base.append(first_primes[i])
+             baseSize += 1
+         i += 1
+     if print_debug:
+         print "base = ",
+         print base
+     # Find baseSize+1 smooth numbers in the interval  [sqrt(n)-M, sqrt(n)+M] with M = B^3
+     M = int(power(B, 3))
+     X = max(1, int(math.ceil(math.sqrt(N)-M)))
+     smooth = []
+     nSmooth = 0
+     T = np.zeros(2*M, dtype=np.long)
+     for i in range(2*M):
+         T[i] = abs((X+i)*(X+i)-N)
+         if T[i] == 1:
+             smooth.append(X+i)
+             nSmooth += 1
+         if T[i] == 0:
+             return X+i, X+i
+     if print_debug:
+         print "T = ",
+         print T
+     for p in range(1, baseSize):
+         R = tonelli_shanks(N, base[p])
+         if print_debug:
+             print("p = %d, R = " % base[p]),
+             print R
+         m = X % base[p]
+         for r in R:
+             if r-m >= 0:
+                 i = r-m
+             else:
+                 i = r-m+base[p]
+             i = int(i)
+             while i < 2*M:
+                 if T[i] % base[p] != 0:
+                     print("WTF!!! p = %d, r = %d, i = %d, T[i] = %d" % (base[p], r, i, T[i]))
+                 T[i] /= base[p]
+                 while T[i] % base[p] == 0:
+                     T[i] /= base[p]
+                 if T[i] == 1:
+                     smooth.append(X+i)
+                     nSmooth += 1
+                 i += base[p]
+
+         if nSmooth > p+5:
+             base = base[0:p+1]
+             baseSize = p+1
+             break
+     if print_debug:
+         print "T = ",
+         print T
+         print "smooth = ",
+         print smooth
+         print("baseSize = %d, base = " % baseSize)
+         print base
+
+     if nSmooth <= baseSize:
+         print("Not enough smooth numbers :(")
+
+     dec = np.zeros((nSmooth, baseSize), dtype=np.long)
+     dec_parity = []
+     #dec_parity = np.zeros(nSmooth, dtype=np.long)
+
+     for i in range(nSmooth):
+         if print_debug:
+            print("smooth[i] = %d" % smooth[i])
+         a, b = decompose(smooth[i]*smooth[i]-N, baseSize, base)
+         if print_debug:
+             print a
+             print "{0:b}".format(b)
+         dec[i] = a
+         dec_parity.append(b)
+
+     '''a = int(math.ceil(math.sqrt(N)))
      a_saved = []
      b = a*a-N
      counter = 0
      while counter <= baseSize:
          if print_debug:
-             print a - 42
+             print a
          d, d_parity = decompose(b, baseSize, base)
          if d is not None:
              if print_debug:
@@ -308,24 +516,40 @@ def quadratic_sieve(N):
              dec_parity[counter] = d_parity
              counter += 1
          a += 1
-         b = a*a-N
+         b = a*a-N'''
      # Find even combination
-     comb = gauss_jordan(dec_parity, baseSize)
-     d = np.zeros(baseSize, dtype=np.int)
-     a = 1
-     b = 1
-     for x in comb:
-         if print_debug:
-            print dec[x, :]
-            print a, b
-         a *= a_saved[x]
-         for i in range(baseSize):
-             if dec[x, i] != 0:
-                b *= math.pow(base[i], dec[x, i])
+     combs = gauss_jordan_bitwise(dec_parity, nSmooth, baseSize)
      if print_debug:
-         print a, int(math.sqrt(b))
-     g = gcd(a-int(math.sqrt(b)), N)
-     return g, N/g
+         print "combs = ",
+         print combs
+     # Find non trivial factors
+     for comb in combs:
+         a = 1
+         b = 1
+         tot = np.zeros(baseSize, dtype=np.int)
+         if print_debug:
+             print "comb = ",
+             print "{0:b}".format(comb)
+         x = nSmooth-1
+         while comb != 0:
+             if comb & 1 == 1:
+                 if print_debug:
+                    print "decomposition = ",
+                    print dec[x, :]
+                 a = (a*smooth[x])%N
+                 for i in range(baseSize):
+                     if dec[x, i] != 0:
+                        tot[i] += dec[x, i]
+             comb = comb >> 1
+             x -= 1
+         for i in range(baseSize):
+             b = (b * power(base[i], tot[i]/2)) % N
+         if print_debug:
+             print a, b
+         g = gcd(a-b, N)
+         if g != 1 and g != N:
+             return g, N/g
+
 
 def factorize(N):
     if print_debug:
@@ -411,6 +635,7 @@ def first_factorisations():
             file.write("], ")
     file.close()
 
+
 if __name__ == "__main__":
     #x = 834
     # 836 -> 0.26
@@ -421,7 +646,15 @@ if __name__ == "__main__":
     #    first_factorisations()
     #else:
     #exit(main())
-    N = 1817
+    #find_first_primes()
+    time0 = datetime.now()
+    N = 138762438070143122474623 #130750493899956009090497 #4153755852461398439 #32193886486049401L #253442228047 #152061056489 #152020131149 #7373 #5535333969127 #3691153417681 #10379
     a, b = quadratic_sieve(N)
     print a
     print b
+    elapsed_time_function("main", time0)
+    '''a, b = decompose(84, 4,[2, 3, 5, 7])
+    print a
+    print b
+    print "{0:b}".format(b)'''
+
